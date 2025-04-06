9015daf3507e1ff1ae693bcd62b318aaf79dce1d
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2025-04-06 18:57:50 +0200

    fix: tailwind issue

--- src/components/GuessGame.vue
+++ src/components/GuessGame.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="flex flex-col items-center space-y-4 p-4 max-w-md mx-auto">
-    <h2 class="text-2xl font-semibold">Угадай число от 1 до 100</h2>
-    <input
-      v-model.number="guess"
-      type="number"
-      placeholder="Ваш вариант"
-      class="border px-2 py-1 rounded w-full"
-    />
-    <button
-      @click="checkGuess"
-      class="bg-blue-500 text-white px-4 py-2 rounded"
-    >
-      Проверить
-    </button>
-    <p v-if="message" class="text-lg">{{ message }}</p>
-    <button @click="resetGame" class="text-sm text-gray-500 underline mt-2">
-      Начать заново
-    </button>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="flex flex-col items-center space-y-4 p-4 max-w-md">
+      <h2 class="text-2xl font-semibold text-2xl">Угадай число от 1 до 100</h2>
+      <input
+        v-model.number="guess"
+        type="number"
+        placeholder="Ваш вариант"
+        class="border px-2 py-1 rounded w-full"
+      />
+      <button
+        @click="checkGuess"
+        class="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+         Проверить
+      </button>
+      <p v-if="message" class="text-lg">{{ message }}</p>
+      <button @click="resetGame" class="text-sm text-gray-500 underline mt-2">
+        Начать заново
+      </button>
+    </div>
   </div>
 </template>
 
